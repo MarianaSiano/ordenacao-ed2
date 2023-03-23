bec7ce370f418719364b73222c675bc6c638a21e
Mariana <maryhsiano@hotmail.com>
2023-03-23 17:34:09 -0300

    'InsertionSort.h' feito'

---
+++ Atividade/InsertionSort.h
@@ -0,0 +1,10 @@
+#ifndef INSERTIONSORT_H_INCLUDED
+#define INSERTIONSORT_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+void insertSort(int vetor[], int n);
+void printInsertionSort(int vetor[], int n);
+
+#endif
